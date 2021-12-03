9c7940ae775d1bafa2c099ec85c5db145269d6d0
ollykr <okremianski@gmail.com>
2021-12-03 18:26:33 -0500

    Starting a new game

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 <template>
 <h1>Ninja Reaction Timer</h1>
+<!-- "start" is a function name exported below -->
+<!-- The button is disabled while we are playing, we can't start a new game while current game is on -->
+<button @click="start" :disabled="isPlaying">play</button>
+<!-- Add/display Block component -->
+<!-- Only display after clicking "Play" -->
+<!-- Pass in "delay" property as a prop and bind (v-bind:delaay or just :delay) some data (a "delay" property) to it-->
+<Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
+import Block from './components/Block.vue'
 
 export default {
   name: 'App',
-  components: {}
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null
+    }
+  },
+  methods: {
+    start() {
+      // 2000ms === 2sec <-- random amount
+      // Gives us a random amount of milliseconds between 0 and 5000
+      // lest - 2000ms and most - 7000 (2000 + 5000)
+      // each time we start a game, it is going to be different amont between 2sec and 7sec 
+      this.delay = 2000 + Math.random() * 5000,
+      // when we click a play button, the game starts playing
+      this.isPlaying = true
+    }
+  }
 }
 </script>
 
---
+++ src/components/Block.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="block">Click Me</div>
+</template>
+
+<script>
+export default {
+// Take in a prop "delay" that I want to receive fro App.vue
+props: ['delay'],
+}
+</script>
+
+<style>
+ .block {
+    width: 400px;
+    border-radius: 20px;
+    background:  #0faf87;;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
